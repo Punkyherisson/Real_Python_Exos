3cb2b51e791dbcd7ac0c696d894208d8383419f2
Punkyherisson <emmanuel.goitia@gmail.com>
2025-07-20 01:31:11 +0200

    2025-07-20 chapitre3_pythonbasic

---
+++ chapitre3.py
@@ -0,0 +1,10 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+====== RESTART: C:/Github.com/Punkyherisson/Real_Python_Exos/chapitre3.py ======
+hello world
+>>> greeting="Hello"
+>>> print(greeting)
+Hello
+>>> greeting
+'Hello'
